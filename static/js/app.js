91a2eb5476a0850319d4c5fb7574b9398769ff33
Erin Hislope <erinhislope@Erins-MacBook-Pro.local>
2020-09-26 19:56:43 -0700

    updating app.js, adding filter

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// add data to table
 const tbody = d3.select("tbody");
 
 tableData.forEach((ufoReport) => {
@@ -11,3 +11,22 @@ tableData.forEach((ufoReport) => {
       cell.text(value);
     });
   });
+
+//   add event handler function
+  const runEnter = () => {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    // Get the value property of the input element
+    let inputElement = d3.select(".form-control"), 
+        inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(Date);
+  
+    let filteredData = date.filter(date => date.sighting === inputValue);
+  
+    console.log(filteredData);
+  }
