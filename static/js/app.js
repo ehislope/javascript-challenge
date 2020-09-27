7d339d93dee5384eeb8a6267990d6871cb8518d6
Erin Hislope <erinhislope@Erins-MacBook-Pro.local>
2020-09-26 19:14:50 -0700

    updating app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,12 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+const tbody = d3.select("tbody");
+
+tabaleData.forEach((ufoReport) => {
+    let row = tbody.append("tr");
+    Object.values(ufoReport).forEach(value => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
