1a44e35d1a9b269a4337f41c29782766edea9b34
Erin Hislope <erinhislope@Erins-MacBook-Pro.local>
2020-10-02 17:28:01 -0700

    updating app.js

--- static/js/app.js
+++ static/js/app.js
@@ -6,34 +6,50 @@ form = d3.select("#filters");
 // add data to table
 const tbody = d3.select("tbody");
 
-tableData.forEach((ufoReport) => {
-    let row = tbody.append("tr");
+function tableBuilder(data){
+  tbody.html("");
+  data.forEach((ufoReport) => {
+    const row = tbody.append("tr");
     Object.values(ufoReport).forEach(value => {
       let cell = row.append("td");
       cell.text(value);
     });
   });
-
-//   add event handler function
-  const runEnter = () => {
-
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-    
-    // Select the input element and get the raw HTML node
-    // Get the value property of the input element
-    let inputElement = d3.select("#datetime"),
-    inputValue = inputElement.property("value");
-
-console.log(inputValue);
+}
 console.log(tableData);
 
-let filteredData = tableData.filter(date => date.datetime === inputValue);
+//   add event handler function
+// const runEnter = () => {
 
-console.log(filteredData);
+// }
+   
+
+function runEnter() {
+   // Prevent the page from refreshing
+   d3.event.preventDefault();
+    // Select the input element and get the raw HTML node
+    // Get the value property of the input element
+    let inputElement = d3.select("#datetime").property("value");
+    let filteredData = tableData
+
+
+    console.log(inputElement);
+    console.log(filteredData);
+   
+
+  // let filteredData = tableData.filter(date => date.datetime === inputElement);
+    if (inputElement) {
+      filteredData=filteredData.filter(row => row.datetime ===inputElement);
+
+    }  
+
+    tableBuilder(filteredData);
 
 }
+
 
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit", runEnter);
+
+tableBuilder(tableData);
